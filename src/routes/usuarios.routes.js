ef3e4a1a1ca6be81a0a1343b06d50eed93a89ca5
basiliomartinez <basilioezequielmartinez@gmail.com>
2026-04-24 11:50:49 -0300

    se agregó middleware para verificar JWT y ruta protegida de perfil

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,10 +3,18 @@ import {
   registrarUsuario,
   loginUsuario,
 } from "../controllers/usuarios.controllers.js";
+import verificarJWT from "../middlewares/verificarJWT.js";
 
 const router = Router();
 
 router.post("/registro", registrarUsuario);
 router.post("/login", loginUsuario);
+
+router.get("/perfil", verificarJWT, (req, res) => {
+  res.status(200).json({
+    mensaje: "Ruta protegida OK",
+    usuario: req.usuario,
+  });
+});
 
 export default router;
